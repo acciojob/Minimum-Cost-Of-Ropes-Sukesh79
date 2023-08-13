4112e678a1b96fe1959a546aee80b695e8836ac4
Sukesh Reddy <117748776+Sukesh79@users.noreply.github.com>
2023-08-13 22:43:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function calculateMinCost() {
   //your code here
+
+	let arr = [4,2,7,6,9];
+	arr.sort();
+
+	while(arr.size() > 1){
+		let a = arr.shift();
+		let b = arr.shift();
+		let sum = a + b;
+		let pivotIdx = findPivot(arr, sum);
+
+		arr.splice(pivotIdx, 0, sum);
+	}
+	
+  return arr[0];
   
-  
-  
-}  
+}
+
+function findPivot(arr, num){
+	for(int i = 0; i < arr.length; i++){
+		if(arr[i] > num) return i;
+	}
+
+	return arr.length;
+}
